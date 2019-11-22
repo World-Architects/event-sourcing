e5878672c4db2a17c1f8c7b79f8ade5266457eb9
Florian Krämer <florian.kraemer@kreative-design.net>
2019-11-22 09:53:46 +0100

    Aggregate Trait

---
+++ src/Aggregate/AggregateTrait.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace Psa\EventSourcing\Aggregate;
+
+use Iterator;
+
+/**
+ * Account Aggregate
+ */
+trait AggregateTrait
+{
+	/**
+	 * @var array
+	 */
+	protected $recordedEvents = [];
+
+	/**
+	 * @var int
+	 */
+	protected $aggregateVersion = 0;
+
+	/*
+	 * @var string|object
+	 */
+	protected $aggregateId;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function aggregateId(): string
+	{
+		return (string)$this->aggregateId;
+	}
+
+	/**
+	 * @param object $event Event
+	 * @return void
+	 */
+	protected function recordThat(object $event): void
+	{
+		$this->aggregateVersion++;
+		$this->recordedEvents[] = $event;
+		$this->applyEvent($event);
+	}
+
+	/**
+	 * @param \Iterator $events Events
+	 * @return self
+	 */
+	public static function reconstituteFromHistory(Iterator $events): self
+	{
+		$instance = new self();
+		$instance->replayEvents($events);
+
+		return $instance;
+	}
+
+	/**
+	 * Replays a list of events on this aggregate
+	 *
+	 * It is protected for the reason of not allowing the "public" to access this
+	 * method from the outside. Our event store implementation will take care
+	 * of that by using reflections to call the method anyway when we need to
+	 * add events.
+	 *
+	 * @param \Iterator $events Events
+	 * @return void
+	 */
+	protected function replayEvents(Iterator $events): void
+	{
+		foreach ($events as $event) {
+			$this->aggregateVersion++;
+			$this->applyEvent($event);
+		}
+	}
+
+	/**
+	 * @param object $event Event Object
+	 * @return void
+	 */
+	protected function applyEvent(object $event): void
+	{
+		$classParts = explode('\\', get_class($event));
+		$method = 'when' . end($classParts);
+
+		if (method_exists($this, $method)) {
+			$this->{$method}($event);
+		}
+	}
+}
